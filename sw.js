d2315fb30cf78622b9118f696c567d9cdbaa88dd
SBL2026 <levin.duerig@bluewin.ch>
2026-09-19 00:23:02 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+/* Service Worker: macht das Tool offline nutzbar.
+   Strategie: aus dem Cache antworten, im Hintergrund aktualisieren (neue Version ab dem nächsten Start). */
+const CACHE = 'score-overlay-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(hit => {
+    const net = fetch(e.request).then(res => {
+      if (res && res.ok && new URL(e.request.url).origin === self.location.origin) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => hit);
+    return hit || net;
+  }));
+});
